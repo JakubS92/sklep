15c62dceb6188f18a59e9867fc483e90dd2149ae
Jakub Stawowczyk <konto_do_kont@op.pl>
2015-01-26 23:01:50 -0800

    Wygenerowano schemat bazy danych i entity

---
+++ src/Projekt/SklepBundle/Entity/Movie.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace Projekt\SklepBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Movie
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Projekt\SklepBundle\Entity\MovieRepository")
+ */
+class Movie
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="director", type="string", length=255)
+     */
+    private $director;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal",  precision=10, scale=2)
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imageurl", type="string", length=255)
+     */
+    private $imageurl;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Category")
+     **/
+    private $category;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="stars", type="integer")
+     */
+    private $stars;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Movie
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Movie
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set director
+     *
+     * @param string $director
+     * @return Movie
+     */
+    public function setDirector($director)
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+
+    /**
+     * Get director
+     *
+     * @return string 
+     */
+    public function getDirector()
+    {
+        return $this->director;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Movie
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set imageurl
+     *
+     * @param string $imageurl
+     * @return Movie
+     */
+    public function setImageurl($imageurl)
+    {
+        $this->imageurl = $imageurl;
+
+        return $this;
+    }
+
+    /**
+     * Get imageurl
+     *
+     * @return string 
+     */
+    public function getImageurl()
+    {
+        return $this->imageurl;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \stdClass $category
+     * @return Movie
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \stdClass 
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set stars
+     *
+     * @param integer $stars
+     * @return Movie
+     */
+    public function setStars($stars)
+    {
+        $this->stars = $stars;
+
+        return $this;
+    }
+
+    /**
+     * Get stars
+     *
+     * @return integer 
+     */
+    public function getStars()
+    {
+        return $this->stars;
+    }
+
+    public function __toString(){
+        return $this->title;
+    }
+}
